e4560170dd7598ff160fdea27a370d4433d2481d
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2020-09-17 11:23:17 +0600

    first commit

---
+++ app/src/main/java/com/example/recyclerview_insert_delete_button/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.recyclerview_insert_delete_button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
